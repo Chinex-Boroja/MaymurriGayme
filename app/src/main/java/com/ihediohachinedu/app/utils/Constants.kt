b762fcaba608722f506f6d036ca58685db9711da
Ihedioha Chinedu Innocent <ihediohachinedu21@gmail.com>
2022-02-27 13:03:09 +0100

    first commit

---
+++ app/src/main/java/com/ihediohachinedu/app/utils/Constants.kt
@@ -0,0 +1,18 @@
+package com.ihediohachinedu.app.utils
+
+import com.ihediohachinedu.app.R
+
+val DEFAULT_ICONS = listOf(
+    R.drawable.ic_toys,
+    R.drawable.ic_fire,
+    R.drawable.ic_food_beverage,
+    R.drawable.ic_football,
+    R.drawable.ic_home,
+    R.drawable.ic_shipping,
+    R.drawable.ic_sunny,
+    R.drawable.ic_leak_add,
+    R.drawable.ic_local_florist,
+    R.drawable.ic_visibility,
+    R.drawable.ic_verified,
+    R.drawable.ic_face
+)
