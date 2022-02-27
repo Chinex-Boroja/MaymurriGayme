91bf8971e778a3943f5f9b77c714444c695c78a6
Ihedioha Chinedu Innocent <ihediohachinedu21@gmail.com>
2022-02-27 13:02:15 +0100

    first commit

---
+++ app/src/main/java/com/ihediohachinedu/app/models/BoardSize.kt
@@ -0,0 +1,23 @@
+package com.ihediohachinedu.app.models
+
+enum class BoardSize(val numberOfCards: Int) {
+    EASY(8),
+    MEDIUM(18),
+    HARD(24);
+
+    fun getWidth(): Int {
+        return when (this) {
+            EASY -> 2
+            MEDIUM -> 3
+            HARD -> 4
+        }
+    }
+
+    fun getHeight(): Int {
+        return numberOfCards / getWidth()
+    }
+
+    fun getNumberOfPairs(): Int {
+        return numberOfCards / 2
+    }
+}
