4469972d516cb7cfcdb4456cc56dd8335b08db93
Ihedioha Chinedu Innocent <ihediohachinedu21@gmail.com>
2022-03-01 09:40:46 +0100

    created a memory card state class

---
+++ app/src/main/java/com/ihediohachinedu/app/models/MemoryGame.kt
@@ -0,0 +1,67 @@
+package com.ihediohachinedu.app.models
+
+import com.ihediohachinedu.app.utils.DEFAULT_ICONS
+
+class MemoryGame(private val boardSize: BoardSize) {
+
+    val images: List<MemoryCard>
+    var numberOfPairsFound = 0
+    private var numberOfImageFlip = 0
+
+    private var indexOfSingleSelectedImage: Int? = null
+
+    init {
+        val chosenImages = DEFAULT_ICONS.shuffled().take(boardSize.getNumberOfPairs())
+        val randomizedImages = (chosenImages + chosenImages).shuffled()
+        images = randomizedImages.map { MemoryCard(it, false, false) }
+    }
+
+    fun flipImage(position: Int): Boolean{
+        numberOfImageFlip++
+        val image = images[position]
+        //Three cases
+        //0 cards previously flipped over => restore cards + flip over the selected
+        //1 card previously flipped over => flip over the selected card + check if the images match
+        //2 cards previously flipped over => restore cards + flip over the selected card
+        var foundMatch = false
+
+        if (indexOfSingleSelectedImage == null) {
+            // 0 or 2 cards previously flipped over
+            restoreImages()
+            indexOfSingleSelectedImage = position
+        } else {
+            foundMatch = checkForMatch(indexOfSingleSelectedImage!!, position)
+            indexOfSingleSelectedImage = null
+
+        }
+        image.isFaceUp = !image.isFaceUp
+        return foundMatch
+    }
+
+    private fun checkForMatch(position1: Int, position2: Int): Boolean {
+        if (images[position1].identifier != images[position2].identifier) {
+            return false
+        }
+        images[position1].isMatched = true
+        images[position2].isMatched = true
+        numberOfPairsFound++
+        return true
+    }
+    private fun restoreImages() {
+         for (image in images) {
+             if (!image.isMatched) {
+                 image.isFaceUp = false
+             }
+         }
+    }
+
+    fun haveWonGame(): Boolean {
+        return numberOfPairsFound == boardSize.getNumberOfPairs()
+    }
+    fun isImageFaceUp(position: Int): Boolean {
+        return images[position].isFaceUp
+    }
+    fun getNumberOfMoves(): Int {
+        return numberOfImageFlip / 2
+    }
+}
