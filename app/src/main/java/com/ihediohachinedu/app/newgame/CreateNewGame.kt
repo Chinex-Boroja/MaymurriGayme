26a2ff1d398b3d4d208dcab2b401217cdd71c84f
Ihedioha Chinedu Innocent <ihediohachinedu21@gmail.com>
2022-03-03 10:53:00 +0100

    Class for creating custom game

--- app/src/main/java/com/ihediohachinedu/app/newgame/CreateNewGame.kt
+++ app/src/main/java/com/ihediohachinedu/app/newgame/CreateNewGame.kt
@@ -3,9 +3,16 @@ package com.ihediohachinedu.app.newgame
 import android.app.Activity
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.ImageDecoder
 import android.net.Uri
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.text.Editable
+import android.text.InputFilter
+import android.text.TextWatcher
 import android.util.Log
 import android.view.MenuItem
 import android.widget.Button
@@ -17,9 +24,11 @@ import androidx.recyclerview.widget.RecyclerView
 import com.ihediohachinedu.app.ImageSelectorAdapter
 import com.ihediohachinedu.app.R
 import com.ihediohachinedu.app.models.BoardSize
+import com.ihediohachinedu.app.utils.BitmapScaler
 import com.ihediohachinedu.app.utils.EXTRA_BOARD_SIZE
 import com.ihediohachinedu.app.utils.isPermissionGranted
 import com.ihediohachinedu.app.utils.requestPermission
+import java.io.ByteArrayOutputStream
 
 class CreateNewGame : AppCompatActivity() {
 
@@ -28,6 +37,8 @@ class CreateNewGame : AppCompatActivity() {
          private const val PICK_PHOTO_CODE = 208
          private const val READ_EXTERNAL_PHOTOS_CODE = 343
          private const val READ_PHOTO_PERMISSION = android.Manifest.permission.READ_EXTERNAL_STORAGE
+         private const val MIN_GAME_LENGTH = 4
+         private const val MAX_GAME_LENGTH = 15
      }
 
     //Member variable to be referenced across different files
@@ -54,6 +65,23 @@ class CreateNewGame : AppCompatActivity() {
         boardSize = intent.getSerializableExtra(EXTRA_BOARD_SIZE) as BoardSize
         numberOfImagesRequired = boardSize.getNumberOfPairs()
         supportActionBar?.title = "Select images (0 / $numberOfImagesRequired)"
+
+        //save game to firebase
+        buttonSave.setOnClickListener {
+            saveDataToFirebase()
+        }
+
+        eTGameName.filters = arrayOf(InputFilter.LengthFilter(MAX_GAME_LENGTH))
+        eTGameName.addTextChangedListener(object: TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+                buttonSave.isEnabled = shouldEnableSaveButton()
+            }
+        })
 
         adapter = ImageSelectorAdapter(this, selectedImageUris, boardSize, object: ImageSelectorAdapter.ImageClickListener {
             override fun onPlaceholderClicked() {
@@ -115,6 +143,48 @@ class CreateNewGame : AppCompatActivity() {
         }
         adapter.notifyDataSetChanged()
         supportActionBar?.title = "Select images (${selectedImageUris.size} / $numberOfImagesRequired)"
+        buttonSave.isEnabled = shouldEnableSaveButton()
+    }
+
+    private fun saveDataToFirebase() {
+        //Logic for downscaling the images for the games
+        Log.i(TAG, "saveDataToFirebase")
+        //iterate over the selected image Uris with index and elements are imageUri
+        for ((index, imageUri) in selectedImageUris.withIndex())  {
+            //imageByteArray is going to be uploaded to Firebase
+            val imageByteArray = getImageByteArray(imageUri)
+
+        }
+    }
+
+    //This method is going to take care of all the downgrading
+    private fun getImageByteArray(imageUri: Uri): ByteArray {
+        //get the original bitmap, based on the photo Uri depending on the Api version of the phone
+        val originalBitmap = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+            val source = ImageDecoder.createSource(contentResolver, imageUri)
+            ImageDecoder.decodeBitmap(source)
+        } else {
+            //If we are running the lower version of the phone
+            MediaStore.Images.Media.getBitmap(contentResolver, imageUri)
+        }
+        Log.i(TAG, "Original width ${originalBitmap.width} and height ${originalBitmap.height}")
+        val scaleBitmap = BitmapScaler.scaleToFitHeight(originalBitmap, 250)
+        Log.i(TAG, "Scaled width ${scaleBitmap.width} and height ${scaleBitmap.height}")
+
+        val byteOutputStream = ByteArrayOutputStream()
+        scaleBitmap.compress(Bitmap.CompressFormat.JPEG, 60, byteOutputStream)
+        return byteOutputStream.toByteArray()
+    }
+
+    private fun shouldEnableSaveButton(): Boolean {
+        // This is to check if we can enable the save button or not
+        if (selectedImageUris.size != numberOfImagesRequired) {
+            return false
+        }
+        if (eTGameName.text.isBlank() || eTGameName.text.length < MIN_GAME_LENGTH) {
+            return false
+        }
+        return true
     }
 
     private fun launchIntentForPhotos() {
