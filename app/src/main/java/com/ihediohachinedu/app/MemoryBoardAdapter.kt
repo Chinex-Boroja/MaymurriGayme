1938fbdfda08e026b5ac38e6b8848a9c087dd14d
Ihedioha Chinedu Innocent <ihediohachinedu21@gmail.com>
2022-03-04 14:16:29 +0100

    added reference from firebase

--- app/src/main/java/com/ihediohachinedu/app/MemoryBoardAdapter.kt
+++ app/src/main/java/com/ihediohachinedu/app/MemoryBoardAdapter.kt
@@ -12,6 +12,7 @@ import androidx.core.view.ViewCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.ihediohachinedu.app.models.BoardSize
 import com.ihediohachinedu.app.models.MemoryCard
+import com.squareup.picasso.Picasso
 import kotlin.math.min
 
 class MemoryBoardAdapter(
@@ -55,7 +56,17 @@ class MemoryBoardAdapter(
 
         fun bind(position: Int) {
             val memoryCard = randomizedImages[position]
-            imageButton.setImageResource(if (memoryCard.isFaceUp) memoryCard.identifier else R.drawable.ic_launcher_background)
+            if (memoryCard.isFaceUp) {
+                if (memoryCard.imageUrl != null) {
+                    //use picasso to render a particular image url inside the imageButton
+                    Picasso.get().load(memoryCard.imageUrl).into(imageButton)
+                }else {
+                    imageButton.setImageResource(memoryCard.identifier)
+                }
+            } else {
+                //if faced down, set the launcher background
+                imageButton.setImageResource(R.drawable.ic_launcher_background)
+            }
             imageButton.alpha = if (memoryCard.isMatched) .4f else 1.0f
             val colorStateList = if (memoryCard.isMatched) ContextCompat.getColorStateList(context, R.color.color_gray) else null
             ViewCompat.setBackgroundTintList(imageButton, colorStateList)
